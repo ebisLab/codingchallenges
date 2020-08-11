8bc587d8186c6511eeef1382522c2aa1b9400d38
ebisLab <ebaiden@techie.com>
2020-08-10 23:29:01 -0400

    LL1

--- hashmap/occurence.py
+++ hashmap/occurence.py
@@ -1,12 +1,23 @@
+import time
+
 arr1 = "AABAACAADAABAABA"
 
 
 def wordcount(arr):
+    t0 = time.time()
     countme = dict()
     for i in arr:
         print("count", i)
         countme[i] = countme.get(i, 0)+1
+        # countme[i] = count
+    t1 = time.time()
+    print(f"The search took {t1-t0} seconds.")
+
     return countme
 
 
 print(wordcount(arr1))
+
+
+large1 = ['nemo' for i in range(5)]
+print(large1)
